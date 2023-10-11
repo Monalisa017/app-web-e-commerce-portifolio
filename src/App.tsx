f58282bc0205d0f85df1c54e83b901ec1ddae29e
Monalisa017 <monalisa.silva@aluno.faculdadeimpacta.com.br>
2023-10-11 17:08:47 -0300

    feat: Adicionado react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 
 export const App = () => {
   return (
-    <div className="App">
-      tESTE 2.0
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+    
   );
 }
 
